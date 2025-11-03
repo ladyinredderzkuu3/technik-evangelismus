942727d6faaad63909e63e2197f54dff55dc2d40
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-11-03 04:38:41 -0600

    feat: initial commit for setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='technik_evangelismus',
+    version='0.1',
+    packages=find_packages(),
+    description='Ein Projekt zur Integration moderner Technologien im Bildungsbereich.',
+    author='Technik Evangelismus Team',
+    author_email='team@technik-evangelismus.org',
+    license='MIT',
+    install_requires=[],
+)
